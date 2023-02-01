0def66b4998eb7766f7bedf3c6dca499ebae3800
malwana <77true@rambler.ru>
2023-02-01 11:55:21 +0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CheckGPT_RestAPI
- CheckGPT is a neural network to detect content generated using big LMs (like ChatGPT, GPT3, GPT2). For AI generated content detection, we are using statistical and heuristical methods, perplexity, entropy, coherence and consistency and other. Accuracy is up to 98%. Use our web app and RestAPI at checkgpt.app.
+CheckGPT is a neural network to detect content generated using big LMs (like ChatGPT, GPT3, GPT2). 
+For AI generated content detection, we are using statistical and heuristical methods, perplexity, entropy, coherence and consistency and other. 
+Accuracy is up to 98%. 
+
+Use our web app and RestAPI at checkgpt.app.
+
+
+# Usage via steemlit webapp:
+![alt text](https://img3.teletype.in/files/e6/b4/e6b48b09-6c0a-408e-b65f-ffbfc86718cc.png![img_1.png](img_1.png))
+
+# Check content via Telegram bot https://t.me/chatgpt_bot
+
+_Example of usage:_
+![alt text](https://img3.teletype.in/files/e6/b4/e6b48b09-6c0a-408e-b65f-ffbfc86718cc.png)
+
+![alt text](https://img1.teletype.in/files/80/49/8049d6bf-29e5-4c93-b1c6-545e389ec8b6.png)
+![alt text](https://img4.teletype.in/files/b6/e2/b6e2a43e-2f86-4a3a-9a96-082e81e9a6ed.png)
---
+++ restapi_usage_example.py
@@ -0,0 +1,42 @@
+import json
+import requests
+
+url = "http://checkgpt.app:8000/predict"
+
+data_empl = {
+    "text": """In the one point of view, it is one of the man’s characters to be curious. He wants to break the limits and learn more. No one can oppose to this quality of human. This curiosity caused many inventions and profits for the people around the world. In the other hand, we live together and we should think about others, have compassion for them and give their due. It is not fair that governments notice on their points and don’t think that it may harm other people. In my opinion, governments should spend money for our basic needs on earth and stop spending money for outer space researches.
+If one person deprived himself and his family from eating to buy and run a car, we may consider him mad. It is not acceptable to spend millions of money’s funds and expenditures for space researches while there are many people around the world who have a severe lack of food, water, and hygiene and many other primary necessities. When we see that there is many violent thugs, suicides, robberies and divorces occur in our society and most of these problems are because of lack of money or poverty, It is not conventional to squander this huge amount of money for traveling to space while many essential requirements have not answered yet.
+Many times, we see from our television or read from news that our earth is in a very bad condition. Deforesting caused many jungles destroyed; air pollution is going to be a very dangerous problem and so on. It is utterly absurd that governments waste this amount of money while our earth, the only place for us to live; situation deteriorates every day so fast.
+At last but not the least, there are still many troubles in our society and earth in that many people die because of lack of food and water supply in the 20th century what is not a negligible problem. Furthermore, our earth is going to be an intolerable place to live providing we don’t find out a solution for it. It’s better to government focus their money on the people and earth problems first and then let space look after itself."""
+}
+
+data = {
+    "text": """The websites I refer to do not have a specific date of update, but some resources you can refer to are:
+
+Wikipedia (Last updated on 2023-01-30)
+Philosophy Now (Last updated on 2021)
+Stanford Encyclopedia of Philosophy (Last updated on 2022)
+Intellectual Enlightenment refers to the development of reason, rationality, and critical thinking. It's the process of acquiring knowledge and understanding through the use of reason and logic.
+
+Spiritual Enlightenment refers to a state of being where one experiences a deep sense of inner peace, interconnectedness with the universe, and an understanding of the true nature of reality. It often involves a spiritual awakening and a transformation of consciousness.
+
+In short, Intellectual Enlightenment is about the growth of knowledge and understanding, while Spiritual Enlightenment is about the growth of the self and the inner experience.
+"""
+}
+
+
+def send_req(data, is_debug=True):
+    jsoned_req = json.dumps(data)
+    if is_debug: print(jsoned_req)
+    r = requests.post(url, data=jsoned_req, headers={'Content-Type': 'application/json'})
+    if is_debug: print(r.text)
+    return r
+
+
+if __name__ == "__main__":
+    print('This is CheckGPT - A neural network to detect content generated using big LMs (like ChatGPT, GPT3, GPT2)')
+    print('For AI generated content detection, we are using statistical and heuristical methods, perplexity, entropy, '
+          'coherence and consistency and other. Accuracy is up to 98%. Use our web app and RestAPI at checkgpt.app.')
+    answr = send_req(data)
+    print(answr)
+    print('Done.')
